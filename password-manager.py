300e45f4789f83c9ea69cf1779ca157a7e8b1e5c
Iman Fatima <imanfati745@gmail.com>
2025-05-31 06:17:36 -0700

    Update password-manager.py

--- password-manager.py
+++ password-manager.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
 
-
+'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)
 
-write_key()
+write_key()'''
 def load_key():
     file = open("key.key", "rb")
     key = file.read()
